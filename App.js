42cd837af5da1b1227189c1b60518eea502785a4
shivamparkar <70848428+shivamparkar@users.noreply.github.com>
2021-07-10 22:46:41 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'React';
-import CardList from './Cardlist';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css'
 
@@ -10,7 +8,6 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
       robots: [],
       searchfield: ''
     }
@@ -22,25 +19,27 @@ class App extends Component {
     .then(user => this.setstate({robots:users}));
   }
 
-    this.setState({ robots: robots});
   }
   onSearchChange = (event) => {
     this.setstate({ searchfield: event.target.value})
   }
-  render() {
+  render()
     const filteredRobots = this.state.robots.filter(robots =>{
       return robots.name.tolowerCase().includes(this.state.searchfield.tolowerCase());
     })
     if (this.state.robots.length === 0){
-    return (
-      <div className ="tc">
-      <h1>Robofriend</h1>
-      <h1 className='f2'>Robofriend</h1>
-      <SearchBox searchchange={this.onSearchChange}/>
-      <CardList robots= {filteredRobots} />
-      </div>
-    );
+      return <h1>Loading ..</h1>
+    }else{
+      return (
+        <div className ="tc">
+         <h1 className='f2'>Robofriend</h1>
+        <SearchBox searchchange={this.onSearchChange}/>
+         <Scroll>
+           <CardList robots= {filteredRobots} />
+        </Scroll>
+        </div>
+      );
+    }
   }
-}
 }
 export default App;
